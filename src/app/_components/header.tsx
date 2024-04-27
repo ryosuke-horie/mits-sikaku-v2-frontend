a63552ecb359af834e8dd951e96826cbf64fbd51
ryosuke-horie <r-horie@mamiya-its.co.jp>
2024-04-27 09:29:10 +0900

    エラーがでるので認証の分岐を削除

--- src/app/_components/header.tsx
+++ src/app/_components/header.tsx
@@ -3,13 +3,9 @@
 import Image from "next/image";
 import Link from "next/link";
 import logoImage from "/public/logo.jpg";
-import { useCookies } from "next-client-cookies";
 
 // ヘッダー用コンポーネント
 export default function Header(): JSX.Element {
-  const cookies = useCookies();
-  // 認証済みかの判定フラグを、クッキーから取得
-  const isAuthenticated = cookies.get("token") ? true : false;
 
   return (
     <div className="h-30 mb-0 flex w-full items-center justify-between bg-white shadow lg:mb-8">
@@ -22,9 +18,7 @@ export default function Header(): JSX.Element {
         </Link>
       </div>
 
-      {/* 認証関連は認証済みの場合にのみ表示する */}
-        {!isAuthenticated && (
-            <div className="mx-4 flex items-center">
+    <div className="mx-4 flex items-center">
             <Link href="/signup">
                 <span className="text-gray-700">新規登録</span>
             </Link>
@@ -33,7 +27,6 @@ export default function Header(): JSX.Element {
                 <span className="text-gray-700">ログイン</span>
             </Link>
             </div>
-            )}
         </div>
   );
 }
