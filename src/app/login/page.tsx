4afcb78a545feb1f043bb90520f6d607630b239f
ryosuke-horie <r-horie@mamiya-its.co.jp>
2024-05-14 12:59:42 +0900

    不要なimportを削除

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,8 +1,6 @@
 "use client";
-import Image from "next/image";
 import { useRouter } from "next/navigation";
 import { useState } from "react";
-import logoImage from "/public/logo.jpg";
 import { useCookies } from "next-client-cookies";
 
 export const runtime = 'edge';
