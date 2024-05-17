f37ff2f7f016aa001ebeafde0186551767ac7f9f
ryosuke-horie <ryosuke.horie37@gmail.com>
2024-05-17 10:21:41 +0900

    experimental-edgeに更新（ビルドエラーのため）

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,7 +1,7 @@
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
-export const runtime = "edge";
+export const runtime = "experimental-edge";
 
 export async function middleware(request: NextRequest) {
   const redirectFlag = request.cookies.get("redirectFlag");
